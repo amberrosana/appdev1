b24bfa3c18ed25257a09e12a3c561e279a3424bf
amberrosana <amberprincessrosana05@gmail.com>
2025-09-06 01:02:47 +0800

    Add modules export activity

---
+++ 11_javascript-refresher/15_modules_export.js
@@ -0,0 +1,18 @@
+/*
+15_modules_export.js
+Create a file that exports:
+A default function that returns a greeting.
+A named constant object containing your information (e.g., name and age).
+(No starter code for this one – create from scratch.)
+*/
+
+export default function greet() {
+    return `Hello there~`
+}
+
+export const myInfo = {
+    name: "Amber Rosana",
+    age: 19,
+    hobby: "listening to music",
+    country: "Philippines"
+}
