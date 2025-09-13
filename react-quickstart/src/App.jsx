19d8297826227aa71cd9c1e8c0805557119cf69c
amberrosana <amberprincessrosana05@gmail.com>
2025-09-13 10:48:54 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -15,8 +15,13 @@ function AdminPanel() {
 function AboutPage() {
   return(
     <>
-      <h1>This is the About Page</h1>
+      <h1 style={{color: "aquamarine"}}>This is the About Page</h1>
       <p>This is where we can put our company details. <br/> Like its history, mission, vision, etc. </p>
+      <img 
+        alt="logo"
+        src="https://i.pinimg.com/1200x/30/fd/63/30fd63cfd90bd7bfeda55fae612f97e8.jpg"
+        className="logo"
+      />
     </>
   )
 }
