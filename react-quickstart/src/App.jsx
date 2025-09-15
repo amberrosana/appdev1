e22aa39280a92122d75839d79286e95ff55ac20e
amberrosana <amberprincessrosana05@gmail.com>
2025-09-16 00:56:52 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -6,6 +6,14 @@ function MyButton() {
   );
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
 
 export default function MyApp() {
   return (
