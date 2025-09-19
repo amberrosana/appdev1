550a936974f24ea8dce5f00c6a7f377bb6b7aaa9
amberrosana <amberprincessrosana05@gmail.com>
2025-09-20 06:50:18 +0800

    Done rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,102 +1,44 @@
 import './App.css'
 
-function MyButton() {
+
+function App() {
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+
+const listItems = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+);
+
   return (
-    <button>I'm a button</button>
+    <ul>{listItems}</ul>
   );
 }
 
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
   );
-}
 
-function MyApp() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <ul>{listItems}</ul>
   );
-}
-
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
-
-function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  );
-}
-
-function AdminPanel() {
-  return (
-    <h1>This is the Admin Panel</h1>
-  )
-}
-
-function LoginForm() {
-  return (
-    <h1>Login Form</h1>
-  )
-}
-
-// Conditional Rendering using if...else statement
-function App1() {
-  let content;
-  let isLoggedIn = false;
-
-  if (isLoggedIn) {
-    content = <AdminPanel />;
-  } else {
-    content = <LoginForm />;
-  }
-
-  return (
-    <div>
-      {content} 
-    </div>
-  );
-}
-
-// Conditional Rendering using Conditional (ternary) operator
-function App2() {
-  let isLoggedIn = true;
-  return (
-    <div>
-      {isLoggedIn ? (
-        <AdminPanel />
-      ) : (
-        <LoginForm />
-      )}
-  </div>
-  )
-}
-
-// Conditional Rendering using logical && syntax
-export default function App3() {
-  let isLoggedIn = true;
-  return (
-    <div>
-      {isLoggedIn && <AdminPanel />}
-    </div>
-  )
 }
