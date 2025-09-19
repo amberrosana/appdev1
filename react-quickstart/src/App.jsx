d5240d913497d5324d449c6ccd5dbd4309cb686a
amberrosana <amberprincessrosana05@gmail.com>
2025-09-20 07:12:30 +0800

    Done sharing data between components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -61,7 +61,15 @@ function ShoppingList() {
 
 // UPDATNG THE SCREEN / USING HOOKS
 
-function MyButton() {
+function MyButton({ count, onClick }) {
+  return (
+	  <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+export default function MyApp() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
@@ -69,18 +77,10 @@ function MyButton() {
   }
 
   return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
-}
-
-export default function MyApp() {
-  return (
     <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
